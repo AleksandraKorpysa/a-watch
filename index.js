193b729032cc2aa865c6d05bc9b585368cbee0f3
Aleksandra Korpysa <a.korpysa1@gmail.com>
2019-11-19 23:13:43 +0100

    changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-<script type="text/javascript">
-    var today = new Date();
-    
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    
-    var hour = today.getHours();
-    var minute = today.getMinutes();
-    var second = today.getSeconds();
-</script>
+
+var today = new Date();
+
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
