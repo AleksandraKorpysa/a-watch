49d7c5caf26281cbc0c194b7485a177de5247bf3
Aleksandra Korpysa <a.korpysa1@gmail.com>
2019-11-19 23:07:41 +0100

    Add projects

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-var today = new Date();
-
-var day = today.getDate();
-var month = today.getMonth() + 1;
-var year = today.getFullYear();
-
-var hour = today.getHours();
-var minute = today.getMinutes();
-var second = today.getSeconds();
-
+<script type="text/javascript">
+    var today = new Date();
+    
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+    var year = today.getFullYear();
+    
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+</script>
