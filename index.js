4b627a3d9ca0e73a32c22c0f213bdb5a042ace44
Aleksandra Korpysa <a.korpysa1@gmail.com>
2019-11-19 21:43:38 +0100

    add readme text

---
+++ index.js
@@ -0,0 +1,10 @@
+var today = new Date();
+
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
